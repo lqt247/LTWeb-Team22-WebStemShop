7e90708784faf4092d48cb7aaeed5f5a8dfd185d
Phan Van Thi <23130309@st.hcmuaf.edu.vn>
2025-11-08 22:29:56 +0700

    add product detail sample data (first 3 products)

--- src/assets/js/pages/product-detail.js
+++ src/assets/js/pages/product-detail.js
@@ -1,57 +1,5 @@
 import { listProduct } from './data.js';
 
-// Lấy ID sản phẩm từ URL
-function getProductIdFromUrl() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('id');
-}
-
-// Tìm sản phẩm theo ID
-function findProductById(id) {
-    return listProduct.find(product => product.idProduct === id);
-}
-
-// Render chi tiết sản phẩm
-function renderProductDetail(product) {
-    const productDetailHtml = `
-        <div class="product-images">
-            <img src="${product.img}" alt="${product.title}" class="main-image">
-            <div class="thumbnail-images">
-                <img src="${product.img}" alt="${product.title}">
-                <!-- Thêm ảnh thumbnail khác nếu có -->
-            </div>
-        </div>
-        <div class="product-info">
-            <p class="product-trademarke">${product.trademarke}</p>
-            <h1>${product.title}</h1>
-            <div class="product-price">${product.price}</div>
-            <div class="product-description">
-                <p>Mô tả chi tiết sản phẩm sẽ được hiển thị ở đây...</p>
-            </div>
-            <div class="product-actions">
-                <div class="quantity-selector">
-                    <button onclick="decreaseQuantity()">-</button>
-                    <input type="number" id="quantity" value="1" min="1">
-                    <button onclick="increaseQuantity()">+</button>
-                </div>
-                <button class="add-to-cart">
-                    Thêm vào giỏ hàng <i class="fa-solid fa-cart-plus"></i>
-                </button>
-                <button class="wishlist-btn">
-                    <i class="fa-regular fa-heart"></i>
-                </button>
-            </div>
-            <div class="product-meta">
-                <p><strong>Thương hiệu:</strong> ${product.trademarke}</p>
-                <p><strong>ID Sản phẩm:</strong> ${product.idProduct}</p>
-                <!-- Thêm thông tin khác nếu cần -->
-            </div>
-        </div>
-    `;
-    
-    document.getElementById('product-detail').innerHTML = productDetailHtml;
-}
-
 
 // Xử lý số lượng sản phẩm
 window.decreaseQuantity = function() {
--- src/assets/js/pages/shop.js
+++ src/assets/js/pages/shop.js
@@ -1,337 +1,144 @@
+//import { listProduct } from "./data.js";
 
-// const listProduct = [
-//     {
-//         idProduct: "1",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "2",
-//         img: "../../assets/images/shop/do-choi-qua-tao-hoc-chu.png\" alt=\"do-choi-qua-tao-hoc-chu",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Quả Táo Học Chữ ABC VTECH 80-139060",
-//         price: "969.000 Đ"
-//     },
-//     {
-//         idProduct: "3",
-//         img: "../../assets/images/shop/nat-geo-bo-khai-quat-kho-bau-vang.png\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "STEAM",
-//         title: "Đồ Chơi Nat Geo Bộ Khai Quật Kho Báu Vàng STEAM RTNGDOUBDIG",
-//         price: "299.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/do-choi-trang-diem-dang-balo-gau-dau.png\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "SWEET HEART",
-//         title: "Đồ chơi trang điểm dạng balo gấu dâu",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "5",
-//         img: "../../assets/images/shop/bot-nan-4-mau.png\" alt=\"bot-nan-4-mau",
-//         trademarke: "PLAYDOH",
-//         title: "Combo bột nặn 4 màu và bột nặn 4 màu mini PLAYDOH CBB5517-23241-33",
-//         price: "497.200Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/dung-cu-choi-cat.png\" alt=\"dung-cu-choi-cat",
-//         trademarke: "VTECH",
-//         title: "bộ cát, dụng cụ và khay chơi cát kinetic sand 6024397",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },    {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },    {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },    {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//     {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//         {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//         {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//         {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//         {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//         {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//         {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
-//         {
-//         idProduct: "4",
-//         img: "../../assets/images/shop/xe-tap-di-da-nang.jpg\" alt=\"xe-tap-di-da-nang",
-//         trademarke: "VTECH",
-//         title: "Đồ Chơi Xe Tập Đi Đa Năng Có Nhạc Và Đèn Cho Bé VTECH 80-505600",
-//         price: "1.779.000 Đ"
-//     },
+// const pageSize = 10; // Số sản phẩm muốn hiển thị trên 1 trang
+// let currentPage = 1; // Trang hiện tại
+// const totalPages = Math.ceil(listProduct.length / pageSize); // Tổng số trang cần có
 
-// ]
+// // --- LẤY CÁC PHẦN TỬ HTML ---
+// const productCardContainer = document.getElementById("list-product__body__card");
+// const paginationContainer = document.querySelector(".list-product__body__pagination");
+// const prevButton = paginationContainer.querySelector('.fa-rotate-180');
+// const nextButton = paginationContainer.querySelector('.fa-play');
+// const pageNumbersList = paginationContainer.querySelector('ul');
 
-import { listProduct } from "./data.js";
+// // Make viewDetail a global function
+// window.viewDetail = function(id) {
+//     window.location.href = `product-detail.html?id=${id}`;
+// };
+// // RENDER SẢN PHẨM ---
+// function renderProducts(page) {
+//     currentPage = page; // Cập nhật trạng thái trang hiện tại
 
-const pageSize = 10; // Số sản phẩm muốn hiển thị trên 1 trang
-let currentPage = 1; // Trang hiện tại
-const totalPages = Math.ceil(listProduct.length / pageSize); // Tổng số trang cần có
+//     // 1. Tính toán vị trí bắt đầu và kết thúc của mảng con
+//     const startIndex = (currentPage - 1) * pageSize;
+//     const endIndex = currentPage * pageSize;
 
-// --- LẤY CÁC PHẦN TỬ HTML ---
-const productCardContainer = document.getElementById("list-product__body__card");
-const paginationContainer = document.querySelector(".list-product__body__pagination");
-const prevButton = paginationContainer.querySelector('.fa-rotate-180');
-const nextButton = paginationContainer.querySelector('.fa-play');
-const pageNumbersList = paginationContainer.querySelector('ul');
+//     // 2. Cắt mảng để lấy sản phẩm của trang hiện tại
+//     const productsForPage = listProduct.slice(startIndex, endIndex);
 
-// Make viewDetail a global function
-window.viewDetail = function(id) {
-    window.location.href = `product-detail.html?id=${id}`;
-};
-// RENDER SẢN PHẨM ---
-function renderProducts(page) {
-    currentPage = page; // Cập nhật trạng thái trang hiện tại
+//     // 3. Tạo chuỗi HTML và chèn vào DOM
+//     const listCardHTML = productsForPage.map((item) =>
+//         ` 
+//         <div class="list-product__body__card__container" data-id="${item.idProduct}">
+//             <img src="${item.img}" alt="${item.title}">
+//             <div class="list-product__body__card__content">    
+//                 <p>${item.trademarke}</p>
+//                 <div class="list-product__body__card__content__title">
+//                     <h5>${item.title}</h5>
+//                 </div>
+//                 <h4>${item.price}</h4>
+//                 <div class="list-product__body__card__addCard">
+//                     <button>Thêm vào giỏ hàng</button>
+//                     <i class="fa-regular fa-heart"></i>
+//                 </div>
+//                 <div class="list-product__body__card__evaluate">
+//                     <i class="fa-solid fa-star"></i>
+//                     <i class="fa-solid fa-star"></i>
+//                     <i class="fa-solid fa-star"></i>
+//                     <i class="fa-solid fa-star"></i>
+//                     <i class="fa-solid fa-star"></i>
+//                 </div>
+//             </div>
+//         </div>
+//         `
+//     ).join("");
 
-    // 1. Tính toán vị trí bắt đầu và kết thúc của mảng con
-    const startIndex = (currentPage - 1) * pageSize;
-    const endIndex = currentPage * pageSize;
-
-    // 2. Cắt mảng để lấy sản phẩm của trang hiện tại
-    const productsForPage = listProduct.slice(startIndex, endIndex);
-
-    // 3. Tạo chuỗi HTML và chèn vào DOM
-    const listCardHTML = productsForPage.map((item) =>
-        ` 
-        <div class="list-product__body__card__container" data-id="${item.idProduct}">
-            <img src="${item.img}" alt="${item.title}">
-            <div class="list-product__body__card__content">    
-                <p>${item.trademarke}</p>
-                <div class="list-product__body__card__content__title">
-                    <h5>${item.title}</h5>
-                </div>
-                <h4>${item.price}</h4>
-                <div class="list-product__body__card__addCard">
-                    <button>Thêm vào giỏ hàng</button>
-                    <i class="fa-regular fa-heart"></i>
-                </div>
-                <div class="list-product__body__card__evaluate">
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                </div>
-            </div>
-        </div>
-        `
-    ).join("");
-
-    productCardContainer.innerHTML = listCardHTML;
+//     productCardContainer.innerHTML = listCardHTML;
     
-    // Add click event listeners
-    document.querySelectorAll('.list-product__body__card__container').forEach(card => {
-        card.addEventListener('click', function() {
-            const id = this.getAttribute('data-id');
-            viewDetail(id);
-        });
-    });
-}
+//     // Add click event listeners
+//     document.querySelectorAll('.list-product__body__card__container').forEach(card => {
+//         card.addEventListener('click', function() {
+//             const id = this.getAttribute('data-id');
+//             viewDetail(id);
+//         });
+//     });
+// }
+
+// // --- RENDER THANH PHÂN TRANG ---
+// function renderPagination() {
+//     pageNumbersList.innerHTML = ''; // Xóa các nút trang cũ
+
+//     // Hiển thị tối đa 5 nút trang (hoặc tùy bạn)
+//     const maxVisiblePages = 5;
+//     let startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
+//     let endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
+
+//     // Điều chỉnh lại startPage nếu cần
+//     if (endPage - startPage + 1 < maxVisiblePages) {
+//         startPage = Math.max(1, endPage - maxVisiblePages + 1);
+//     }
+
+//     // 1. Thêm nút ... ở đầu nếu cần
+//     if (startPage > 1) {
+//         pageNumbersList.innerHTML += '<li>...</li>';
+//     }
+
+//     // 2. Thêm các nút số trang
+//     for (let i = startPage; i <= endPage; i++) {
+//         const li = document.createElement('li');
+//         li.textContent = i;
+//         li.classList.add('page-item');
+
+//         // Đánh dấu trang hiện tại
+//         if (i === currentPage) {
+//             li.style.backgroundColor = '#FF6C80';
+//             li.style.color = 'white';
+//         }
+
+//         // Thêm sự kiện click
+//         li.addEventListener('click', () => {
+//             renderProducts(i);
+//             renderPagination(); // Render lại thanh phân trang sau khi chuyển trang
+//         });
+//         pageNumbersList.appendChild(li);
+//     }
+
+//     // 3. Thêm nút ... ở cuối nếu cần
+//     if (endPage < totalPages) {
+//         pageNumbersList.innerHTML += '<li>...</li>';
+//     }
+
+//     // 4. Xử lý trạng thái nút PREV/NEXT
+//     prevButton.style.opacity = currentPage === 1 ? '0.5' : '1';
+//     prevButton.style.pointerEvents = currentPage === 1 ? 'none' : 'auto';
+
+//     nextButton.style.opacity = currentPage === totalPages ? '0.5' : '1';
+//     nextButton.style.pointerEvents = currentPage === totalPages ? 'none' : 'auto';
+// }
 
 
+// // --- CHUYỂN TRANG (PREV/NEXT) ---
+// prevButton.addEventListener('click', () => {
+//     if (currentPage > 1) {
+//         renderProducts(currentPage - 1);
+//         renderPagination();
+//     }
+// });
 
-// --- RENDER THANH PHÂN TRANG ---
-function renderPagination() {
-    pageNumbersList.innerHTML = ''; // Xóa các nút trang cũ
-
-    // Hiển thị tối đa 5 nút trang (hoặc tùy bạn)
-    const maxVisiblePages = 5;
-    let startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
-    let endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
-
-    // Điều chỉnh lại startPage nếu cần
-    if (endPage - startPage + 1 < maxVisiblePages) {
-        startPage = Math.max(1, endPage - maxVisiblePages + 1);
-    }
-
-    // 1. Thêm nút ... ở đầu nếu cần
-    if (startPage > 1) {
-        pageNumbersList.innerHTML += '<li>...</li>';
-    }
-
-    // 2. Thêm các nút số trang
-    for (let i = startPage; i <= endPage; i++) {
-        const li = document.createElement('li');
-        li.textContent = i;
-        li.classList.add('page-item');
-
-        // Đánh dấu trang hiện tại
-        if (i === currentPage) {
-            li.style.backgroundColor = '#FF6C80';
-            li.style.color = 'white';
-        }
-
-        // Thêm sự kiện click
-        li.addEventListener('click', () => {
-            renderProducts(i);
-            renderPagination(); // Render lại thanh phân trang sau khi chuyển trang
-        });
-        pageNumbersList.appendChild(li);
-    }
-
-    // 3. Thêm nút ... ở cuối nếu cần
-    if (endPage < totalPages) {
-        pageNumbersList.innerHTML += '<li>...</li>';
-    }
-
-    // 4. Xử lý trạng thái nút PREV/NEXT
-    prevButton.style.opacity = currentPage === 1 ? '0.5' : '1';
-    prevButton.style.pointerEvents = currentPage === 1 ? 'none' : 'auto';
-
-    nextButton.style.opacity = currentPage === totalPages ? '0.5' : '1';
-    nextButton.style.pointerEvents = currentPage === totalPages ? 'none' : 'auto';
-}
+// nextButton.addEventListener('click', () => {
+//     if (currentPage < totalPages) {
+//         renderProducts(currentPage + 1);
+//         renderPagination();
+//     }
+// });
 
 
-// --- CHUYỂN TRANG (PREV/NEXT) ---
-prevButton.addEventListener('click', () => {
-    if (currentPage > 1) {
-        renderProducts(currentPage - 1);
-        renderPagination();
-    }
-});
-
-nextButton.addEventListener('click', () => {
-    if (currentPage < totalPages) {
-        renderProducts(currentPage + 1);
-        renderPagination();
-    }
-});
-
-
-// --- KHỞI CHẠY LẦN ĐẦU KHI TRANG LOAD XONG ---
-document.addEventListener("DOMContentLoaded", () => {
-    // Chỉ gọi hàm này sau khi các thành phần DOM đã được load
-    renderProducts(currentPage);
-    renderPagination();
-});
+// // --- KHỞI CHẠY LẦN ĐẦU KHI TRANG LOAD XONG ---
+// document.addEventListener("DOMContentLoaded", () => {
+//     // Chỉ gọi hàm này sau khi các thành phần DOM đã được load
+//     renderProducts(currentPage);
+//     renderPagination();
+// });
 
 // -------------------------kiểu xem (view type) ----------------------
 const type1 = document.getElementById('head__type1');
@@ -390,3 +197,24 @@ iconPlusDoChoiLapRap.addEventListener('click', () => {
 
 })
 
+// ------------------chuyển trang productdetail----------------------
+document.addEventListener("DOMContentLoaded", () => {
+    // 1. TÌM TẤT CẢ CÁC THẺ SẢN PHẨM
+    const productCards = document.querySelectorAll('.list-product__body__card__container');
+
+    // 2. DUYỆT QUA TỪNG THẺ VÀ GẮN SỰ KIỆN CLICK
+    productCards.forEach(card => {
+        card.addEventListener('click', function() {
+            // Lấy đường dẫn từ thuộc tính data-url mà bạn đã viết trong HTML
+            const url = this.getAttribute('data-url');
+
+            // Nếu có đường dẫn thì chuyển trang
+            if (url) {
+                window.location.href = url;
+            } else {
+                // Thông báo nếu sản phẩm chưa có trang demo (tùy chọn)
+                alert("Sản phẩm này đang cập nhật nội dung!");
+            }
+        });
+    });
+});
