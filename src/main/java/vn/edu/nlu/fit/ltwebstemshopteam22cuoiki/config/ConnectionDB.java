6319d513dab0af70f103158938b3040b908c9f9e
Phan Van Thi <23130309@st.hcmuaf.edu.vn>
2026-01-02 23:07:02 +0700

    thêm jdbc driver, kết nối với db

---
+++ src/main/java/vn/edu/nlu/fit/ltwebstemshopteam22cuoiki/config/ConnectionDB.java
@@ -0,0 +1,16 @@
+package vn.edu.nlu.fit.ltwebstemshopteam22cuoiki.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionDB {
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/steamshop?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
