3263636d2cbf9720f9472a9fc865ca60109a6f63
thanhtuan2701 <20130456@st.hcmuaf.edu.vn>
2026-01-24 12:11:26 +0700

    tạo util email để dùng email vs app password để gửi mã verify cùng mật khẩu khi nhập đúng email

--- src/main/java/vn/edu/nlu/fit/ltwebstemshopteam22cuoiki/util/EmailUtil.java
+++ src/main/java/vn/edu/nlu/fit/ltwebstemshopteam22cuoiki/util/EmailUtil.java
@@ -6,7 +6,7 @@ import jakarta.mail.internet.*;
 import java.util.Properties;
 
 public class EmailUtil {
-
+        // xác thực email
     public static void sendVerifyEmail(String toEmail, String verifyLink) {
 
         final String fromEmail = "nhoktizike@gmail.com";
@@ -38,5 +38,41 @@ public class EmailUtil {
             e.printStackTrace();
         }
     }
+
+    // gửi mật khẩu mới
+    public static void sendResetPasswordEmail(String toEmail, String newPassword) {
+        final String fromEmail = "nhoktizike@gmail.com";
+        final String password = "ibym uosw rize kbjh";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject("Cấp lại mật khẩu StemShop");
+
+            message.setText(
+                    "Mật khẩu mới của bạn là: " + newPassword +
+                            "\nVui lòng đăng nhập và đổi mật khẩu ngay."
+            );
+
+            Transport.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
 
