3d0e9e64ba751cc8dc491736b0cd613b0162362f
Lê Quang Trường <23130355@st.hcmuaf.edu.vn>
2026-01-29 10:24:26 +0700

    23130355 le quang truong (#24)
    * Cập nhật Trang chủ (cho phép render dữ liệu 6 sản phẩm từ db, update index, link), thêm HomeServlet, cập nhật lại header, footer, hàm getFirst6product

--- src/main/java/vn/edu/nlu/fit/ltwebstemshopteam22cuoiki/model/Category.java
+++ src/main/java/vn/edu/nlu/fit/ltwebstemshopteam22cuoiki/model/Category.java
@@ -19,10 +19,6 @@ public class Category {
         return categoryName;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
